56627fd8771e29b04d691f1474dfe7f61ebb975b
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 18:08:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,8 @@ class Triangle
   def kind 
     if (@length_1 <= 0 || @length_2 <= 0 || @length_3 <= 0) || @length_2 + @length_3 <= @length_1 || @length_1 + @length_3 <= @length_2 || @length_1 + @length_2 <= @length_3
       raise TriangleError
+      elsif @length_1 == @length_2 && @length_2 == @length_3
+      
   end 
   
   class TriangleError < StandardError 
