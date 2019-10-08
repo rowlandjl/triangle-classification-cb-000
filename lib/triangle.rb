f3a3eedccc39fa58d33a68ed8a32eecfc42651b4
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 18:14:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,7 @@ class Triangle
     elsif @length_1 == @length_2 && @length_2 == @length_3
       :equilateral 
     elsif @length_2 == @length_3 || @length_1 == @length_3 || @length_1 == @length_2
+      :isosceles 
     end 
   end 
   
