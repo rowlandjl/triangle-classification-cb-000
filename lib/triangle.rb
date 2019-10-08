9fd31575378a48dbd7744dff9434760fd9204073
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 18:06:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end 
   
   def kind 
-    if (@length_1 <= 0 || @length_2 <= 0 || @length_3 <= 0) || @length_2 + @length_3 
+    if (@length_1 <= 0 || @length_2 <= 0 || @length_3 <= 0) || @length_2 + @length_3 <= @length_1
   end 
   
   class TriangleError < StandardError 
