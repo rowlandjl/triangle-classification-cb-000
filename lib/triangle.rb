9c9246560ea8739a3a8990aa3921ea2ff84648ff
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 17:53:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,1 @@
-class Triangle
-  
-  def initialize(side_1, side_2, side_3)
-    # @side_1 = side_1
-    # @side_2 = side_2
-    # @side_3 = side_3
-  end 
-  
-  TriangleError < StandardError
-  end 
-  
-end
+
